9f0367b71b8188c1a5f476aeb6cb9081300679c6
paul <pauldurbz@gmail.com>
2015-11-01 18:04:31 +0000

    Created the main plugin class and the plugin.yml

---
+++ src/main/java/com/ithinkrok/mccw/WarsPlugin.java
@@ -0,0 +1,22 @@
+package com.ithinkrok.mccw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by paul on 01/11/15.
+ *
+ * The main plugin class for Colony Wars
+ */
+public class WarsPlugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+}

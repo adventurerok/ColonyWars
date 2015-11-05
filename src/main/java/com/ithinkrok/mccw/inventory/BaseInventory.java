0b7a2a6aab41e954458d81cf4456d273c0b35dce
paul <pauldurbz@gmail.com>
2015-11-05 16:00:52 +0000

    Bases will sell blacksmiths and magetowers if the team has a farm

--- src/main/java/com/ithinkrok/mccw/inventory/BaseInventory.java
+++ src/main/java/com/ithinkrok/mccw/inventory/BaseInventory.java
@@ -8,43 +8,74 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by paul on 04/11/15.
- *
+ * <p>
  * InventoryHandler for base shops
  */
 public class BaseInventory implements InventoryHandler {
+
+    private int farmCost = 3000;
+    private int blacksmithCost = 4000;
+    private int magetowerCost = 4000;
 
     @Override
     public void onInventoryClick(ItemStack item, PlayerInfo playerInfo, TeamInfo teamInfo) {
         PlayerInventory inv = playerInfo.getPlayer().getInventory();
 
-        switch(item.getItemMeta().getDisplayName()){
+        if (inv.firstEmpty() == -1) {
+            playerInfo.getPlayer().sendMessage("Please ensure you have one free slot in your inventory");
+            return;
+        }
+
+        ItemStack add = null;
+        int cost = 0;
+
+        switch (item.getItemMeta().getDisplayName()) {
             case "Farm":
-                if(!InventoryUtils.hasTeamCash(3000, teamInfo, playerInfo)){
-                    playerInfo.getPlayer().sendMessage("You don't have that amount of money!");
-                    break;
-                }
-
-                HashMap<Integer, ItemStack> leftOver = inv.addItem(InventoryUtils.createItemWithNameAndLore(Material
-                        .OBSIDIAN, 1, 0, "Farm", "Builds a farm when placed!"));
-
-                if(!leftOver.isEmpty()) break;
-
-                InventoryUtils.payWithTeamCash(3000, teamInfo, playerInfo);
-                InventoryUtils.playBuySound(playerInfo.getPlayer());
-
+                cost = farmCost;
+                add = InventoryUtils
+                        .createItemWithNameAndLore(Material.LAPIS_ORE, 1, 0, "Farm", "Builds a farm when placed!");
+                break;
+            case "Blacksmith":
+                cost = blacksmithCost;
+                add = InventoryUtils.createItemWithNameAndLore(Material.LAPIS_ORE, 1, 0, "Blacksmith",
+                        "Builds a blacksmith when placed!");
+                break;
+            case "MageTower":
+                cost = magetowerCost;
+                add = InventoryUtils.createItemWithNameAndLore(Material.LAPIS_ORE, 1, 0, "MageTower",
+                        "Builds a MageTower when placed!");
                 break;
         }
+
+        if (cost == 0 || add == null) return;
+
+        if (!InventoryUtils.hasTeamCash(cost, teamInfo, playerInfo)) {
+            playerInfo.getPlayer().sendMessage("You don't have that amount of money!");
+            return;
+        }
+
+        inv.addItem(add);
+
+        InventoryUtils.payWithTeamCash(cost, teamInfo, playerInfo);
+        InventoryUtils.playBuySound(playerInfo.getPlayer());
     }
 
     @Override
     public List<ItemStack> getInventoryContents(PlayerInfo playerInfo, TeamInfo teamInfo) {
         ArrayList<ItemStack> result = new ArrayList<>();
-        result.add(InventoryUtils.createShopItem(Material.LAPIS_ORE, 1, 0, "Farm", "Build a farm!", 3000, true));
+        result.add(InventoryUtils.createShopItem(Material.LAPIS_ORE, 1, 0, "Farm", "Build a farm!", farmCost, true));
+
+        if (teamInfo.getBuildingCount("Farm") > 0) {
+            result.add(InventoryUtils
+                    .createShopItem(Material.LAPIS_ORE, 1, 0, "Blacksmith", "Build a blacksmith!", blacksmithCost,
+                            true));
+            result.add(InventoryUtils
+                    .createShopItem(Material.LAPIS_ORE, 1, 0, "MageTower", "Build a MageTower!", magetowerCost, true));
+        }
         return result;
     }
 }

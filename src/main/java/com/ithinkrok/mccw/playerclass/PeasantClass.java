d022a9d8a509ddb5cbdba31c96e3fa7debc87a4a
paul <pauldurbz@gmail.com>
2015-11-19 17:15:45 +0000

    Converted the peasant class to the class item system

--- src/main/java/com/ithinkrok/mccw/playerclass/PeasantClass.java
+++ src/main/java/com/ithinkrok/mccw/playerclass/PeasantClass.java
@@ -1,77 +1,31 @@
 package com.ithinkrok.mccw.playerclass;
 
-import com.ithinkrok.mccw.data.Team;
-import com.ithinkrok.mccw.data.User;
-import com.ithinkrok.mccw.event.*;
-import com.ithinkrok.mccw.inventory.BuyableInventory;
-import com.ithinkrok.mccw.inventory.UpgradeBuyable;
+import com.ithinkrok.mccw.playerclass.items.ClassItem;
+import com.ithinkrok.mccw.playerclass.items.LinearCalculator;
 import com.ithinkrok.mccw.strings.Buildings;
-import com.ithinkrok.mccw.util.InventoryUtils;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by paul on 12/11/15.
- *
+ * <p>
  * Handles the peasant class
  */
-public class PeasantClass extends BuyableInventory implements PlayerClassHandler {
+public class PeasantClass extends ClassItemClassHandler {
 
     public PeasantClass(FileConfiguration config) {
-        super(new UpgradeBuyable(InventoryUtils
-                .createItemWithEnchantments(Material.WOOD_SWORD, 1, 0, "Sharpness Upgrade 1", null,
-                        Enchantment.DAMAGE_ALL, 1), Buildings.LUMBERMILL, config.getInt("costs.peasant.sharpness1"),
-                "sharpness", 1), new UpgradeBuyable(InventoryUtils
-                .createItemWithEnchantments(Material.WOOD_SWORD, 1, 0, "Sharpness Upgrade 2", null,
-                        Enchantment.DAMAGE_ALL, 2), Buildings.LUMBERMILL, config.getInt("costs.peasant.sharpness2"),
-                "sharpness", 2), new UpgradeBuyable(InventoryUtils
-                .createItemWithEnchantments(Material.WOOD_SWORD, 1, 0, "Knockback Upgrade 1", null,
-                        Enchantment.KNOCKBACK, 1), Buildings.LUMBERMILL, config.getInt("costs.peasant.knockback1"),
-                "knockback", 1), new UpgradeBuyable(InventoryUtils
-                .createItemWithEnchantments(Material.WOOD_SWORD, 1, 0, "Knockback Upgrade 2", null,
-                        Enchantment.KNOCKBACK, 2), Buildings.LUMBERMILL, config.getInt("costs.peasant.knockback2"),
-                "knockback", 2));
+        super(new ClassItem(Material.IRON_AXE, "Peasant Axe").withUnlockOnGameStart(true).withEnchantmentEffects(
+                new ClassItem.EnchantmentEffect(Enchantment.DIG_SPEED, "axe", new LinearCalculator(2, 0))),
+                new ClassItem(Material.WOOD_SWORD, null).withUpgradeBuildings(Buildings.LUMBERMILL)
+                        .withUnlockOnBuildingBuild(true).withEnchantmentEffects(
+                        new ClassItem.EnchantmentEffect(Enchantment.DAMAGE_ALL, "sharpness",
+                                new LinearCalculator(0, 1)),
+                        new ClassItem.EnchantmentEffect(Enchantment.KNOCKBACK, "knockback", new LinearCalculator(0, 1)))
+                        .withUpgradables(new ClassItem.Upgradable("sharpness", "Sharpness Upgrade %s", 2,
+                                        configArrayCalculator(config, "costs.peasant.sharpness", 2)),
+                                new ClassItem.Upgradable("knockback", "Knockback Upgrade %s", 2,
+                                        configArrayCalculator(config, "costs.peasant.knockback", 2))));
     }
 
-    @Override
-    public void onBuildingBuilt(UserTeamBuildingBuiltEvent event) {
-        if(!Buildings.LUMBERMILL.equals(event.getBuilding().getBuildingName())) return;
-
-        event.getUserInventory().addItem(new ItemStack(Material.WOOD_SWORD));
-    }
-
-    @Override
-    public void onUserBeginGame(UserBeginGameEvent event) {
-        event.getUserInventory().addItem(InventoryUtils.createItemWithEnchantments(Material.IRON_AXE,
-                1, 0, "Peasant Axe", "Better than a usual axe!", Enchantment.DIG_SPEED, 2));
-    }
-
-    @Override
-    public boolean onInteract(UserInteractEvent event) {
-        return false;
-    }
-
-    @Override
-    public void onPlayerUpgrade(UserUpgradeEvent event) {
-        User user = event.getUser();
-
-        switch (event.getUpgradeName()) {
-            case "sharpness":
-            case "knockback":
-                ItemStack sword = new ItemStack(Material.WOOD_SWORD);
-                InventoryUtils.enchantItem(sword, Enchantment.DAMAGE_ALL, user.getUpgradeLevel("sharpness"),
-                        Enchantment.KNOCKBACK, user.getUpgradeLevel("knockback"));
-
-                InventoryUtils.replaceItem(event.getUserInventory(), sword);
-
-                break;
-        }
-    }
-
-    @Override
-    public void onUserAttack(UserAttackEvent event) {
-
-    }
 }

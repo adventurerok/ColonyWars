8fc1a3a65268aabf3b92c86d0d902dd7756931ca
paul <pauldurbz@gmail.com>
2015-11-19 17:21:18 +0000

    Converted the dark knight class to the class item system

--- src/main/java/com/ithinkrok/mccw/playerclass/DarkKnightClass.java
+++ src/main/java/com/ithinkrok/mccw/playerclass/DarkKnightClass.java
@@ -1,114 +1,37 @@
 package com.ithinkrok.mccw.playerclass;
 
-import com.ithinkrok.mccw.event.*;
-import com.ithinkrok.mccw.inventory.BuyableInventory;
-import com.ithinkrok.mccw.inventory.UpgradeBuyable;
+import com.ithinkrok.mccw.event.UserBeginGameEvent;
+import com.ithinkrok.mccw.playerclass.items.ArrayCalculator;
+import com.ithinkrok.mccw.playerclass.items.ClassItem;
 import com.ithinkrok.mccw.strings.Buildings;
-import com.ithinkrok.mccw.util.InventoryUtils;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 /**
  * Created by paul on 14/11/15.
- *
+ * <p>
  * Handles the dark knight class
  */
-public class DarkKnightClass extends BuyableInventory implements PlayerClassHandler {
+public class DarkKnightClass extends ClassItemClassHandler {
 
     public DarkKnightClass(FileConfiguration config) {
-        super(new UpgradeBuyable(InventoryUtils.createItemWithNameAndLore(Material.IRON_HELMET,
-                1, 0, "Darkness Sword Upgrade 1", "Damage: 3.0 Hearts", "Nausea Duration: 7 seconds",
-                "Wither Duration: 6 seconds"), Buildings.MAGETOWER, config.getInt("costs.dark_knight.sword1"),
-                "sword", 1),
-                new UpgradeBuyable(InventoryUtils.createItemWithNameAndLore(Material.IRON_HELMET,
-                        1, 0, "Darkness Sword Upgrade 2", "Damage: 5.0 Hearts", "Nausea Duration: 8 seconds",
-                        "Wither Duration: 10 seconds"), Buildings.MAGETOWER, config.getInt("costs.dark_knight.sword2"),
-                        "sword", 2));
-    }
-
-    @Override
-    public void onBuildingBuilt(UserTeamBuildingBuiltEvent event) {
-        switch(event.getBuilding().getBuildingName()){
-            case Buildings.MAGETOWER:
-                event.getUserInventory().addItem(InventoryUtils.createItemWithNameAndLore(Material.IRON_HELMET,
-                        1, 0, "Darkness Sword", "Damage: 2.0 Hearts", "Nausea Duration: 5 seconds",
-                        "Wither Duration: 3 seconds"));
-                break;
-        }
+        super(new ClassItem(Material.IRON_HELMET, "Darkness Sword").withUpgradeBuildings(Buildings.MAGETOWER)
+                .withUnlockOnBuildingBuild(true).withWeaponModifier(
+                        new ClassItem.WeaponModifier("sword").withDamageCalculator(new ArrayCalculator(4, 6, 10))
+                                .withWitherCalculator(new ArrayCalculator(3, 6, 10))
+                                .withNauseaCalculator(new ArrayCalculator(5, 7, 8))).withUpgradables(
+                        new ClassItem.Upgradable("sword", "Darkness Sword Upgrade %s", 2,
+                                configArrayCalculator(config, "costs.dark_knight.sword", 2))));
     }
 
     @Override
     public void onUserBeginGame(UserBeginGameEvent event) {
-        event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1, false,
-                false), true);
-    }
+        super.onUserBeginGame(event);
 
-    @Override
-    public boolean onInteract(UserInteractEvent event) {
-        return false;
-    }
-
-    @Override
-    public void onPlayerUpgrade(UserUpgradeEvent event) {
-        if(!event.getUpgradeName().equals("sword")) return;
-
-        ItemStack sword;
-        switch(event.getUpgradeLevel()){
-            case 1:
-                sword = InventoryUtils.createItemWithNameAndLore(Material.IRON_HELMET,
-                        1, 0, "Darkness Sword", "Damage: 3.0 Hearts", "Nausea Duration: 7 seconds",
-                        "Wither Duration: 6 seconds");
-                break;
-            case 2:
-                sword = InventoryUtils.createItemWithNameAndLore(Material.IRON_HELMET,
-                        1, 0, "Darkness Sword", "Damage: 5.0 Hearts", "Nausea Duration: 8 seconds",
-                        "Wither Duration: 10 seconds");
-                break;
-            default:
-                return;
-        }
-
-        InventoryUtils.replaceItem(event.getUserInventory(), sword);
-    }
-
-    @Override
-    public void onUserAttack(UserAttackEvent event) {
-        ItemStack item = event.getItem();
-        if (item == null || item.getType() != Material.IRON_HELMET) return;
-
-        double damage;
-        int nausea;
-        int wither;
-
-        switch(event.getUser().getUpgradeLevel("sword")){
-            case 0:
-                damage = 4;
-                nausea = 100;
-                wither = 60;
-                break;
-            case 1:
-                damage = 6;
-                nausea = 140;
-                wither = 120;
-                break;
-            case 2:
-                damage = 10;
-                nausea = 160;
-                wither = 200;
-                break;
-            default:
-                return;
-        }
-
-        event.setDamage(damage);
-        if(!(event.getClickedEntity() instanceof LivingEntity)) return;
-        ((LivingEntity)event.getClickedEntity()).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, nausea, 0,
-                false, true), true);
-        if(event.isAttackingUser()) event.getTargetUser().setWitherTicks(event.getUser(), wither);
-        else ((LivingEntity)event.getClickedEntity()).addPotionEffect(new PotionEffect(PotionEffectType.WITHER, wither, 0));
+        event.getPlayer()
+                .addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1, false, false),
+                        true);
     }
 }

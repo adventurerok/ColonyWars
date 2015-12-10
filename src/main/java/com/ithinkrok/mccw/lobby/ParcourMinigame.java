46c772204d982f2f21ce2418a28970b8a002ede3
paul <pauldurbz@gmail.com>
2015-12-10 19:26:33 +0000

    Parkour money is capped at 2500

--- src/main/java/com/ithinkrok/mccw/lobby/ParcourMinigame.java
+++ src/main/java/com/ithinkrok/mccw/lobby/ParcourMinigame.java
@@ -8,12 +8,13 @@ import org.bukkit.entity.Entity;
 
 /**
  * Created by paul on 18/11/15.
- *
+ * <p>
  * Handles the parcour minigame
  */
 public class ParcourMinigame implements LobbyMinigame {
 
     WarsPlugin plugin;
+    private int maxParkourMoney = 2500;
 
     public ParcourMinigame(WarsPlugin plugin) {
         this.plugin = plugin;
@@ -41,23 +42,30 @@ public class ParcourMinigame implements LobbyMinigame {
 
     @Override
     public boolean onUserInteractWorld(User user, Block block) {
-        switch(block.getType()){
+        switch (block.getType()) {
             case ENDER_CHEST:
-                plugin.messageAllLocale("minigames.parcour.winner", user.getFormattedName(), 500);
-                user.addPlayerCash(500);
-                user.teleport(plugin.getLobbySpawn());
-
+                addParkourMoney(user, 500, false);
                 return true;
             case REDSTONE_LAMP_OFF:
-                plugin.messageAllLocale("minigames.parcour.advanced", user.getFormattedName(), 2000);
-                user.addPlayerCash(2000);
-                user.teleport(plugin.getLobbySpawn());
-
+                addParkourMoney(user, 2000, true);
                 return true;
             default:
                 return false;
         }
 
 
+    }
+
+    private void addParkourMoney(User user, int amount, boolean advanced) {
+        amount = Math.min(amount, maxParkourMoney - user.getPlayerCash());
+
+        if (amount <= 0) user.sendLocale("minigames.parcour.capped");
+        else {
+            user.addPlayerCash(amount);
+            if (advanced) plugin.messageAllLocale("minigames.parcour.advanced", user.getFormattedName(), amount);
+            else plugin.messageAllLocale("minigames.parcour.winner", user.getFormattedName(), amount);
+        }
+
+        user.teleport(plugin.getLobbySpawn());
     }
 }

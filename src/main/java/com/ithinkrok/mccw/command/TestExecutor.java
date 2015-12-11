19c934eca3d827ead5e1e32812f95f9d0a30a2d7
paul <pauldurbz@gmail.com>
2015-12-11 14:20:50 +0000

    The test command now has its own executor

---
+++ src/main/java/com/ithinkrok/mccw/command/TestExecutor.java
@@ -0,0 +1,117 @@
+package com.ithinkrok.mccw.command;
+
+import com.ithinkrok.mccw.data.Team;
+import com.ithinkrok.mccw.data.User;
+import com.ithinkrok.mccw.enumeration.PlayerClass;
+import com.ithinkrok.mccw.enumeration.TeamColor;
+import com.ithinkrok.mccw.util.item.InventoryUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by paul on 11/12/15.
+ *
+ * Handles the /test command
+ */
+public class TestExecutor implements WarsCommandExecutor {
+
+    @Override
+    public boolean onCommand(WarsCommandSender sender, Command command, String label, String[] args) {
+        String playerName = "Console";
+        User user = null;
+
+        if (args.length > 0 && args[args.length - 1].startsWith("-p")) {
+            playerName = args[args.length - 1].substring(2);
+            Player targetPlayer = Bukkit.getPlayer(playerName);
+
+            if (targetPlayer != null) user = sender.getPlugin().getUser(targetPlayer);
+        }
+
+        if(user == null) {
+            sender.sendLocale("commands.test.unknown-player", playerName);
+            return true;
+        }
+
+        switch (args[0]) {
+            case "team":
+                if (args.length < 2) return false;
+
+                TeamColor teamColor = TeamColor.fromName(args[1]);
+                if (teamColor == null) {
+                    user.sendLocale("commands.test.invalid-team", args[1]);
+                    return true;
+                }
+                user.setTeamColor(teamColor);
+
+                user.sendLocale("commands.test.team-change", teamColor.getFormattedName());
+
+                break;
+            case "class":
+                if (args.length < 2) return false;
+
+                PlayerClass playerClass = PlayerClass.fromName(args[1]);
+                if (playerClass == null) {
+                    user.sendLocale("commands.test.invalid-class", args[1]);
+                    return true;
+                }
+                user.setPlayerClass(playerClass);
+
+                user.sendLocale("commands.test.class-change", playerClass.getFormattedName());
+
+                break;
+            case "money":
+
+                user.addPlayerCash(10000);
+                user.getTeam().addTeamCash(10000);
+
+                user.sendLocale("commands.test.money", 10000);
+                break;
+            case "build":
+                if (args.length < 2) return false;
+
+                user.getPlayerInventory()
+                        .addItem(InventoryUtils.createItemWithNameAndLore(Material.LAPIS_ORE, 16, 0, args[1]));
+
+                user.sendLocale("commands.test.build-blocks", 16, args[1]);
+                break;
+            case "base_location":
+                Team team = user.getTeam();
+                if (team == null) {
+                    user.sendLocale("commands.test.base.no-team");
+                    break;
+                }
+
+                user.sendLocale("commands.test.base.loc", team.getBaseLocation());
+                break;
+            case "shrink":
+                if (user.getPlugin().getShowdownArena() == null) return true;
+                if (args.length < 2) return false;
+
+                try {
+                    int amount = Integer.parseInt(args[1]);
+
+                    if (amount < 1 || amount > 30) {
+                        user.sendLocale("commands.shrink.bad-size", args[1]);
+                        return true;
+                    }
+
+                    for (int count = 0; count < amount; ++count) {
+                        user.getPlugin().getShowdownArena().shrinkArena(user.getPlugin());
+                    }
+
+                    return true;
+                } catch (NumberFormatException e) {
+                    user.sendLocale("commands.shrink.bad-size", args[1]);
+                    return true;
+                }
+            case "rejoin":
+                user.getPlugin().getGameInstance().setupUser(user);
+                return true;
+
+        }
+
+        return true;
+    }
+}
--- src/main/java/com/ithinkrok/mccw/listener/CommandListener.java
+++ src/main/java/com/ithinkrok/mccw/listener/CommandListener.java
@@ -1,32 +1,17 @@
 package com.ithinkrok.mccw.listener;
 
 import com.ithinkrok.mccw.WarsPlugin;
+import com.ithinkrok.mccw.command.TestExecutor;
 import com.ithinkrok.mccw.command.WarsCommandExecutor;
 import com.ithinkrok.mccw.command.WarsCommandSender;
 import com.ithinkrok.mccw.command.WarsConsoleSender;
 import com.ithinkrok.mccw.command.executors.*;
-import com.ithinkrok.mccw.data.Team;
-import com.ithinkrok.mccw.data.User;
-import com.ithinkrok.mccw.data.UserCategoryStats;
-import com.ithinkrok.mccw.enumeration.GameState;
-import com.ithinkrok.mccw.enumeration.PlayerClass;
-import com.ithinkrok.mccw.enumeration.TeamColor;
-import com.ithinkrok.mccw.handler.CountdownHandler;
-import com.ithinkrok.mccw.util.item.InventoryUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.util.Vector;
 
-import java.text.DecimalFormat;
-import java.util.*;
+import java.util.HashMap;
 
 /**
  * Created by paul on 13/11/15.
@@ -53,6 +38,7 @@ public class CommandListener implements CommandExecutor {
         executorHashMap.put("countdown", new CountdownExecutor());
         executorHashMap.put("teamchat", new TeamChatExecutor());
         executorHashMap.put("stats", new StatsExecutor());
+        executorHashMap.put("test", new TestExecutor());
     }
 
     @Override
@@ -65,119 +51,7 @@ public class CommandListener implements CommandExecutor {
             else warsCommandSender = consoleSender;
 
             return executorHashMap.get(commandName).onCommand(warsCommandSender, command, label, args);
-        } else return onCommandOld(sender, command, label, args);
+        } else return false;
     }
-
-    public boolean onCommandOld(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(plugin.getLocale("command.no-console"));
-            return true;
-        }
-
-        Player player = (Player) sender;
-        User user = plugin.getUser(player);
-
-        switch (command.getName().toLowerCase()) {
-            case "test":
-                return args.length >= 1 && onTestCommand(user, args);
-            default:
-                return false;
-        }
-
-    }
-
-    private boolean onTestCommand(User user, String[] args) {
-        if (args.length > 0 && args[args.length - 1].startsWith("-p")) {
-            String playerName = args[args.length - 1].substring(2);
-            Player targetPlayer = Bukkit.getPlayer(playerName);
-
-            if (targetPlayer != null) user = plugin.getUser(targetPlayer);
-            else {
-                user.sendLocale("commands.test.unknown-player", playerName);
-                return true;
-            }
-        }
-
-        switch (args[0]) {
-            case "team":
-                if (args.length < 2) return false;
-
-                TeamColor teamColor = TeamColor.fromName(args[1]);
-                if (teamColor == null) {
-                    user.sendLocale("commands.test.invalid-team", args[1]);
-                    return true;
-                }
-                user.setTeamColor(teamColor);
-
-                user.sendLocale("commands.test.team-change", teamColor.getFormattedName());
-
-                break;
-            case "class":
-                if (args.length < 2) return false;
-
-                PlayerClass playerClass = PlayerClass.fromName(args[1]);
-                if (playerClass == null) {
-                    user.sendLocale("commands.test.invalid-class", args[1]);
-                    return true;
-                }
-                user.setPlayerClass(playerClass);
-
-                user.sendLocale("commands.test.class-change", playerClass.getFormattedName());
-
-                break;
-            case "money":
-
-                user.addPlayerCash(10000);
-                user.getTeam().addTeamCash(10000);
-
-                user.sendLocale("commands.test.money", 10000);
-                break;
-            case "build":
-                if (args.length < 2) return false;
-
-                user.getPlayerInventory()
-                        .addItem(InventoryUtils.createItemWithNameAndLore(Material.LAPIS_ORE, 16, 0, args[1]));
-
-                user.sendLocale("commands.test.build-blocks", 16, args[1]);
-                break;
-            case "base_location":
-                Team team = user.getTeam();
-                if (team == null) {
-                    user.sendLocale("commands.test.base.no-team");
-                    break;
-                }
-
-                user.sendLocale("commands.test.base.loc", team.getBaseLocation());
-                break;
-            case "shrink":
-                if (plugin.getShowdownArena() == null) return true;
-                if (args.length < 2) return false;
-
-                try {
-                    int amount = Integer.parseInt(args[1]);
-
-                    if (amount < 1 || amount > 30) {
-                        user.sendLocale("commands.shrink.bad-size", args[1]);
-                        return true;
-                    }
-
-                    for (int count = 0; count < amount; ++count) {
-                        plugin.getShowdownArena().shrinkArena(plugin);
-                    }
-
-                    return true;
-                } catch (NumberFormatException e) {
-                    user.sendLocale("commands.shrink.bad-size", args[1]);
-                    return true;
-                }
-            case "rejoin":
-                plugin.getGameInstance().setupUser(user);
-                return true;
-
-        }
-
-        return true;
-    }
-
 
 }

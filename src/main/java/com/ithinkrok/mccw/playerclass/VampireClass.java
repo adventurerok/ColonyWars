3bec124be27d84eb8c56bba1225a09ef86b21de6
paul <pauldurbz@gmail.com>
2015-12-12 19:16:10 +0000

    Vampires cannot attack while in bat form

--- src/main/java/com/ithinkrok/mccw/playerclass/VampireClass.java
+++ src/main/java/com/ithinkrok/mccw/playerclass/VampireClass.java
@@ -2,6 +2,7 @@ package com.ithinkrok.mccw.playerclass;
 
 import com.ithinkrok.mccw.WarsPlugin;
 import com.ithinkrok.mccw.enumeration.PlayerClass;
+import com.ithinkrok.mccw.event.UserAttackEvent;
 import com.ithinkrok.mccw.playerclass.items.ClassItem;
 import com.ithinkrok.mccw.playerclass.items.LinearCalculator;
 import com.ithinkrok.mccw.strings.Buildings;
@@ -32,11 +33,6 @@ public class VampireClass extends ClassItemClassHandler {
                 new ClassItem.EnchantmentEffect(Enchantment.DAMAGE_ALL, "vampire", new LinearCalculator(0, 1)));
 
         vampireSword.withAttackAction(event -> {
-            if (event.getUser().isCoolingDown("batting")) {
-                event.setCancelled(true);
-                return;
-            }
-
             if (event.getFinalDamage() < 1) return;
             PotionEffect effect = new PotionEffect(PotionEffectType.REGENERATION, 20, 1, false, true);
             event.getUser().getPlayer().addPotionEffect(effect);
@@ -67,5 +63,14 @@ public class VampireClass extends ClassItemClassHandler {
         batWand.withUpgradables(new ClassItem.Upgradable("bat", "upgrades.bat-wand.name", 2));
 
         addExtraClassItems(batWand);
+    }
+
+    @Override
+    public void onUserAttack(UserAttackEvent event) {
+        if (event.getUser().isCoolingDown("batting")) {
+            event.setCancelled(true);
+            return;
+        }
+        super.onUserAttack(event);
     }
 }

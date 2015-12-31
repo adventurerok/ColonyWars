4d1a253b38ab0e18571e1801fecab8bb5160154c
paul <pauldurbz@gmail.com>
2015-12-31 19:08:06 +0000

    The lobby handler cancels block break and place events

--- src/main/java/com/ithinkrok/cw/gamestate/LobbyHandler.java
+++ src/main/java/com/ithinkrok/cw/gamestate/LobbyHandler.java
@@ -2,9 +2,21 @@ package com.ithinkrok.cw.gamestate;
 
 import com.ithinkrok.cw.CWUser;
 import com.ithinkrok.minigames.GameStateHandler;
+import com.ithinkrok.minigames.event.UserBreakBlockEvent;
+import com.ithinkrok.minigames.event.UserPlaceBlockEvent;
 
 /**
  * Created by paul on 31/12/15.
  */
 public class LobbyHandler extends GameStateHandler<CWUser> {
+
+    @Override
+    public void eventBlockBreak(UserBreakBlockEvent<CWUser> event) {
+        event.setCancelled(true);
+    }
+
+    @Override
+    public void eventBlockPlace(UserPlaceBlockEvent<CWUser> event) {
+        event.setCancelled(true);
+    }
 }

ec379a762f92ef594e60a3bb67fef966117be7d1
paul <pauldurbz@gmail.com>
2016-01-01 20:17:46 +0000

    The spleef minigame will allow snow blocks to be broken

--- src/main/java/com/ithinkrok/cw/lobbygames/SpleefMinigame.java
+++ src/main/java/com/ithinkrok/cw/lobbygames/SpleefMinigame.java
@@ -1,7 +1,12 @@
 package com.ithinkrok.cw.lobbygames;
 
+import com.ithinkrok.minigames.User;
 import com.ithinkrok.minigames.event.ConfiguredListener;
+import com.ithinkrok.minigames.event.UserBreakBlockEvent;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 
 /**
  * Created by paul on 01/01/16.
@@ -10,5 +15,10 @@ public class SpleefMinigame implements ConfiguredListener {
     @Override
     public void configure(ConfigurationSection config) {
         System.out.println("Hi Spleef");
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onUserBreakBlock(UserBreakBlockEvent<User> event){
+        if(event.getBlock().getType() == Material.SNOW_BLOCK) event.setCancelled(false);
     }
 }

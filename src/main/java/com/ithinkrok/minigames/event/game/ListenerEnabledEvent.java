363890c92059be46f94ce97efebbe4b7ae1da1ab
paul <pauldurbz@gmail.com>
2016-01-02 11:48:54 +0000

    Changed the usage comment for ListenerEnabledEvent

--- src/main/java/com/ithinkrok/minigames/event/game/ListenerEnabledEvent.java
+++ src/main/java/com/ithinkrok/minigames/event/game/ListenerEnabledEvent.java
@@ -6,7 +6,7 @@ import org.bukkit.configuration.ConfigurationSection;
 /**
  * Created by paul on 02/01/16.
  *
- * Called on a listener when it is enabled (ready and going to start receiving events)
+ * Called on a listener when it is enabled (but not necessarily before it starts receiving events)
  */
 public class ListenerEnabledEvent<G extends GameGroup> extends GameEvent<G> {
 

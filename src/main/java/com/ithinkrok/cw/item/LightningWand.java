d7c84d2db94a4095f0b43b2d66ee8aa879521107
paul <pauldurbz@gmail.com>
2016-01-04 14:59:15 +0000

    Created LightningWand to allow CustomItems to function like original lightning wands

--- src/main/java/com/ithinkrok/cw/item/LightningWand.java
+++ src/main/java/com/ithinkrok/cw/item/LightningWand.java
@@ -1,25 +1,41 @@
 package com.ithinkrok.cw.item;
 
 import com.ithinkrok.minigames.User;
+import com.ithinkrok.minigames.event.ListenerEnabledEvent;
 import com.ithinkrok.minigames.event.user.world.UserAttackEvent;
 import com.ithinkrok.minigames.event.user.world.UserInteractEvent;
 import org.bukkit.block.Block;
 import org.bukkit.entity.LightningStrike;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 /**
  * Created by paul on 04/01/16.
  */
 public class LightningWand implements Listener {
 
+    private double lightingMultiplier;
+    private int maxRange;
+
+    @EventHandler
+    public void onListenerEnabled(ListenerEnabledEvent<?> event) {
+        maxRange = event.getConfig().getInt("max_range");
+        lightingMultiplier = event.getConfig().getDouble("damage_multiplier");
+    }
+
     @EventHandler
     public void onUserAttack(UserAttackEvent<? extends User> event) {
+        if(event.getInteractType() != UserInteractEvent.InteractType.REPRESENTING) return;
 
+        if(event.getDamageCause() == EntityDamageEvent.DamageCause.LIGHTNING) {
+            event.setDamage(event.getDamage() * 2.8);
+        }
     }
 
     @EventHandler
     public void onUserInteract(UserInteractEvent<? extends User> event) {
+        if(event.getInteractType() != UserInteractEvent.InteractType.RIGHT_CLICK) return;
         Block target = event.getUser().rayTraceBlocks(200);
         if(target == null) return;
 

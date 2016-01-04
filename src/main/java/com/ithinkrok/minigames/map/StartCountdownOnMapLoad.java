ab64e34c5105973790676f4f65a4b26c74df076a
paul <pauldurbz@gmail.com>
2016-01-04 21:21:03 +0000

    Created StartCountdownOnMapLoad, which will start a countdown when a map loads

---
+++ src/main/java/com/ithinkrok/minigames/map/StartCountdownOnMapLoad.java
@@ -0,0 +1,24 @@
+package com.ithinkrok.minigames.map;
+
+import com.ithinkrok.minigames.GameGroup;
+import com.ithinkrok.minigames.event.ListenerEnabledEvent;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ * Created by paul on 04/01/16.
+ */
+public class StartCountdownOnMapLoad implements Listener {
+
+    @EventHandler
+    public void onListenerEnabled(ListenerEnabledEvent<GameGroup> event) {
+        ConfigurationSection config = event.getConfig();
+
+        String name = config.getString("name");
+        String localeStub = config.getString("locale_stub");
+        int seconds = config.getInt("seconds");
+
+        event.getCreator().startCountdown(name, localeStub, seconds);
+    }
+}

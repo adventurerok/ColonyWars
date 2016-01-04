10d845bf46c79be0f7288a117b8f246bf163833e
paul <pauldurbz@gmail.com>
2016-01-04 16:03:26 +0000

    Created a listener to give custom items to users when they join

---
+++ src/main/java/com/ithinkrok/minigames/listener/GiveCustomItemsOnJoin.java
@@ -0,0 +1,68 @@
+package com.ithinkrok.minigames.listener;
+
+import com.ithinkrok.minigames.User;
+import com.ithinkrok.minigames.event.ListenerEnabledEvent;
+import com.ithinkrok.minigames.event.user.game.UserJoinEvent;
+import com.ithinkrok.minigames.item.CustomItem;
+import com.ithinkrok.minigames.util.ConfigUtils;
+import com.ithinkrok.minigames.util.math.MapVariables;
+import com.ithinkrok.minigames.util.math.Variables;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by paul on 04/01/16.
+ */
+public class GiveCustomItemsOnJoin implements Listener {
+
+    private boolean clearInventory = false;
+
+    private List<CustomItemInfo> items = new ArrayList<>();
+
+    @EventHandler
+    public void onListenerEnabled(ListenerEnabledEvent event) {
+        ConfigurationSection config = event.getConfig();
+
+        clearInventory = config.getBoolean("clear_inventory", false);
+
+        List<ConfigurationSection> itemConfigs = ConfigUtils.getConfigList(config, "items");
+        items.addAll(itemConfigs.stream().map(CustomItemInfo::new).collect(Collectors.toList()));
+    }
+
+    @EventHandler
+    public void onUserJoin(UserJoinEvent<? extends User> event) {
+        if(clearInventory) event.getUser().getInventory().clear();
+
+        for (CustomItemInfo itemInfo : items) {
+            CustomItem item = event.getUser().getGameGroup().getCustomItem(itemInfo.customItem);
+            ItemStack itemStack;
+
+            if (itemInfo.customVariables != null)
+                itemStack = item.createWithVariables(event.getUser().getGameGroup(), itemInfo.customVariables);
+            else itemStack = event.getUser().createCustomItemForUser(item);
+
+            if(itemInfo.slot < 0) event.getUser().getInventory().addItem(itemStack);
+            else event.getUser().getInventory().setItem(itemInfo.slot, itemStack);
+        }
+    }
+
+    private static class CustomItemInfo {
+        private Variables customVariables;
+        private String customItem;
+        private int slot = -1;
+
+        public CustomItemInfo(ConfigurationSection config) {
+            customItem = config.getString("name");
+            slot = config.getInt("slot", -1);
+
+            if (!config.contains("custom_variables")) return;
+            customVariables = new MapVariables(config.getConfigurationSection("custom_variables"));
+        }
+    }
+}

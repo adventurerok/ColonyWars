9e480f5e9affd90ac6ca8322166cb1efa831db8d
paul <pauldurbz@gmail.com>
2016-01-04 15:37:34 +0000

    WeaponModifier will now add its own lore items

--- src/main/java/com/ithinkrok/minigames/item/WeaponModifier.java
+++ src/main/java/com/ithinkrok/minigames/item/WeaponModifier.java
@@ -4,6 +4,8 @@ import com.ithinkrok.minigames.User;
 import com.ithinkrok.minigames.event.ListenerEnabledEvent;
 import com.ithinkrok.minigames.event.user.world.UserAttackEvent;
 import com.ithinkrok.minigames.event.user.world.UserInteractEvent;
+import com.ithinkrok.minigames.item.event.CustomItemLoreCalculateEvent;
+import com.ithinkrok.minigames.lang.LanguageLookup;
 import com.ithinkrok.minigames.util.math.Calculator;
 import com.ithinkrok.minigames.util.math.ExpressionCalculator;
 import org.bukkit.configuration.ConfigurationSection;
@@ -36,44 +38,65 @@ public class WeaponModifier implements Listener {
 
     @EventHandler
     public void onListenerEnable(ListenerEnabledEvent event) {
-        if(!event.hasConfig()) throw new RuntimeException("A WeaponModifier requires a config");
+        if (!event.hasConfig()) throw new RuntimeException("A WeaponModifier requires a config");
 
         load(event.getConfig());
     }
 
     private void load(ConfigurationSection config) {
-        if(config.contains("damage")) damageCalculator = new ExpressionCalculator(config.getString("damage"));
-        if(config.contains("fire")) fireCalculator = new ExpressionCalculator(config.getString("fire"));
+        if (config.contains("damage")) damageCalculator = new ExpressionCalculator(config.getString("damage"));
+        if (config.contains("fire")) fireCalculator = new ExpressionCalculator(config.getString("fire"));
 
-        if(!config.contains("effects")) return;
+        if (!config.contains("effects")) return;
         ConfigurationSection effects = config.getConfigurationSection("effects");
 
-        for(String effectName : effects.getKeys(false)) {
+        for (String effectName : effects.getKeys(false)) {
             PotionEffectType effectType = PotionEffectType.getByName(effectName);
             effectModifiers.add(new EffectModifier(effectType, effects.getConfigurationSection(effectName)));
+        }
+    }
+
+    @EventHandler
+    public void onLoreItemsCalculate(CustomItemLoreCalculateEvent event) {
+        LanguageLookup lang = event.getLanguageLookup();
+        List<String> lore = event.getLore();
+
+        if (damageCalculator != null)
+            lore.add(lang.getLocale("weapon_modifier.damage", damageCalculator.calculate(event.getVariables())));
+        if (fireCalculator != null)
+            lore.add(lang.getLocale("weapon_modifier.fire", fireCalculator.calculate(event.getVariables())));
+
+        for (EffectModifier modifier : effectModifiers) {
+            double duration = ((int) modifier.durationCalculator.calculate(event.getVariables()) * TICKS_PER_SECOND) /
+                    TICKS_PER_SECOND;
+            int level = (int) modifier.levelCalculator.calculate(event.getVariables());
+            if(duration < 0.05d || level < 1) continue;
+
+            String langName = "weapon_modifier." + modifier.effectType.getName().toLowerCase() + "." + level;
+            lore.add(lang.getLocale(langName, duration));
         }
     }
 
     @SuppressWarnings("unchecked")
     @EventHandler
     public void onUserAttack(UserAttackEvent<? extends User> attack) {
-        if(attack.getInteractType() == UserInteractEvent.InteractType.REPRESENTING) return;
-        if(attack.getDamageCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK) return;
+        if (attack.getInteractType() == UserInteractEvent.InteractType.REPRESENTING) return;
+        if (attack.getDamageCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK) return;
 
-        if(damageCalculator != null) {
+        if (damageCalculator != null) {
             attack.setDamage(damageCalculator.calculate(attack.getUser().getUpgradeLevels()) * HEALTH_PER_HEART);
         }
 
-        if(fireCalculator != null) {
+        if (fireCalculator != null) {
             int fireTicks = (int) (fireCalculator.calculate(attack.getUser().getUpgradeLevels()) * TICKS_PER_SECOND);
 
-            if(fireTicks > 0){
-                if(attack.isAttackingUser()) attack.getTargetUser().setFireTicks(attack.getUser(), fireTicks);
+            if (fireTicks > 0) {
+                if (attack.isAttackingUser()) attack.getTargetUser().setFireTicks(attack.getUser(), fireTicks);
                 else attack.getClickedEntity().setFireTicks(fireTicks);
             }
         }
 
-        for(EffectModifier effectModifier : effectModifiers) {
+        for (EffectModifier effectModifier : effectModifiers) {
             effectModifier.modifyAttack(attack);
         }
     }
@@ -87,7 +110,7 @@ public class WeaponModifier implements Listener {
             this.effectType = effectType;
             durationCalculator = new ExpressionCalculator(config.getString("duration"));
 
-            if(!config.contains("level")) levelCalculator = new ExpressionCalculator("1");
+            if (!config.contains("level")) levelCalculator = new ExpressionCalculator("1");
             else levelCalculator = new ExpressionCalculator(config.getString("level"));
         }
 
@@ -97,12 +120,12 @@ public class WeaponModifier implements Listener {
             int duration = (int) (durationCalculator.calculate(attack.getUser().getUpgradeLevels()) * TICKS_PER_SECOND);
             int amp = (int) (levelCalculator.calculate(attack.getUser().getUpgradeLevels()) - 1);
 
-            if(duration <= 0 || amp < 0) return;
+            if (duration <= 0 || amp < 0) return;
 
-            if(attack.isAttackingUser() && effectType == PotionEffectType.WITHER) {
+            if (attack.isAttackingUser() && effectType == PotionEffectType.WITHER) {
                 attack.getTargetUser().setWitherTicks(attack.getUser(), duration, amp);
             } else {
-                ((LivingEntity)attack.getClickedEntity()).addPotionEffect(new PotionEffect(effectType, duration, amp));
+                ((LivingEntity) attack.getClickedEntity()).addPotionEffect(new PotionEffect(effectType, duration, amp));
             }
         }
     }

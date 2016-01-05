7d0d73f0687d42500fffd4a1c85e098ab141b419
paul <pauldurbz@gmail.com>
2016-01-05 19:23:56 +0000

    Created a listener for the game gamestate

--- src/main/java/com/ithinkrok/cw/gamestate/GameListener.java
+++ src/main/java/com/ithinkrok/cw/gamestate/GameListener.java
@@ -1,9 +1,16 @@
 package com.ithinkrok.cw.gamestate;
 
+import com.ithinkrok.minigames.event.ListenerLoadedEvent;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 /**
  * Created by paul on 05/01/16.
  */
 public class GameListener implements Listener {
+
+    @EventHandler
+    public void onListenerLoaded(ListenerLoadedEvent<?> event) {
+
+    }
 }

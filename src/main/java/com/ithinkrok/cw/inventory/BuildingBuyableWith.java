293f50a2f4082755fb526eb111423d35eb65af50
paul <pauldurbz@gmail.com>
2016-01-13 19:24:50 +0000

    Added BuildingBuyableWith and the missing buildings to the Base shop

---
+++ src/main/java/com/ithinkrok/cw/inventory/BuildingBuyableWith.java
@@ -0,0 +1,36 @@
+package com.ithinkrok.cw.inventory;
+
+import com.ithinkrok.cw.metadata.CWTeamStats;
+import com.ithinkrok.minigames.inventory.event.BuyablePurchaseEvent;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+/**
+ * Created by paul on 13/01/16.
+ */
+public class BuildingBuyableWith extends BuildingBuyable {
+
+    private List<String> with;
+
+    public BuildingBuyableWith(ItemStack baseDisplay) {
+        super(baseDisplay);
+    }
+
+    @Override
+    public void configure(ConfigurationSection config) {
+        with = config.getStringList("with_buildings");
+    }
+
+    @Override
+    public boolean canBuy(BuyablePurchaseEvent event) {
+        CWTeamStats teamStats = CWTeamStats.getOrCreate(event.getUser().getTeam());
+
+        for(String buildingName : with){
+            if(teamStats.getBuildingCount(buildingName) < 1) return false;
+        }
+
+        return true;
+    }
+}

accd3da97721dc1f1f1b52e687b9dc61bc9c9848
paul <pauldurbz@gmail.com>
2016-01-21 18:22:06 +0000

    Fixed a bug due to not removing custom item identifiers

--- src/main/java/com/ithinkrok/minigames/inventory/ItemBuyable.java
+++ src/main/java/com/ithinkrok/minigames/inventory/ItemBuyable.java
@@ -5,6 +5,7 @@ import com.ithinkrok.minigames.inventory.event.BuyablePurchaseEvent;
 import com.ithinkrok.minigames.inventory.event.CalculateItemForUserEvent;
 import com.ithinkrok.minigames.item.CustomItem;
 import com.ithinkrok.minigames.util.ConfigUtils;
+import com.ithinkrok.minigames.util.InventoryUtils;
 import com.ithinkrok.minigames.util.math.Calculator;
 import com.ithinkrok.minigames.util.math.ExpressionCalculator;
 import org.bukkit.configuration.ConfigurationSection;
@@ -16,7 +17,7 @@ import java.util.Map;
 /**
  * Created by paul on 10/01/16.
  */
-public class ItemBuyable extends Buyable{
+public class ItemBuyable extends Buyable {
 
     protected ItemStack purchase;
     protected String customItem;
@@ -34,12 +35,12 @@ public class ItemBuyable extends Buyable{
 
         ItemStack purchase = ConfigUtils.getItemStack(config, "item");
 
-        if(purchase == null && config.contains("custom_item")) {
+        if (purchase == null && config.contains("custom_item")) {
             customItem = config.getString("custom_item");
             amount = new ExpressionCalculator(config.getString("amount", "1"));
-        } else if(purchase != null){
+        } else if (purchase != null) {
             this.purchase = purchase.clone();
-            if(baseDisplay == null) baseDisplay = purchase.clone();
+            if (baseDisplay == null) baseDisplay = purchase.clone();
         }
 
         noSpaceLocale = config.getString("no_inventory_space_locale", "item_buyable.no_space");
@@ -47,8 +48,14 @@ public class ItemBuyable extends Buyable{
 
     @Override
     public void onCalculateItem(CalculateItemForUserEvent event) {
-        if(customItem != null) {
-            event.setDisplay(event.getUserGameGroup().getCustomItem(customItem).createForUser(event.getUser()));
+        if (event.getDisplay() == null && customItem != null) {
+            ItemStack item = event.getUserGameGroup().getCustomItem(customItem).createForUser(event.getUser());
+
+            item.setAmount((int) (item.getAmount() * amount.calculate(event.getUser().getUpgradeLevels())));
+
+            item = InventoryUtils.removeIdentifier(item);
+
+            event.setDisplay(item);
         }
 
         super.onCalculateItem(event);
@@ -56,7 +63,7 @@ public class ItemBuyable extends Buyable{
 
     @Override
     public boolean onPurchase(BuyablePurchaseEvent event) {
-        if(customItem == null) return giveUserItem(event.getUser(), purchase);
+        if (customItem == null) return giveUserItem(event.getUser(), purchase);
         else {
             CustomItem customItem = event.getUserGameGroup().getCustomItem(this.customItem);
             ItemStack item = customItem.createForUser(event.getUser());
@@ -71,11 +78,11 @@ public class ItemBuyable extends Buyable{
         PlayerInventory inv = user.getInventory();
 
         Map<Integer, ItemStack> failedItems = inv.addItem(purchase.clone());
-        if(failedItems.isEmpty()) return true;
+        if (failedItems.isEmpty()) return true;
 
         int removeAmount = purchase.getAmount() - failedItems.get(0).getAmount();
 
-        if(removeAmount != 0) {
+        if (removeAmount != 0) {
             ItemStack remove = purchase.clone();
             remove.setAmount(removeAmount);
 

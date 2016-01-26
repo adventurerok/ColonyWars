701311a1e0fc515f2f493e7d9b333d024e8223e0
paul <pauldurbz@gmail.com>
2016-01-26 19:59:45 +0000

    Renamed MinigamesPlugin to SpecificPlugin

--- src/main/java/com/ithinkrok/cw/CWPlugin.java
+++ src/main/java/com/ithinkrok/cw/CWPlugin.java
@@ -2,14 +2,14 @@ package com.ithinkrok.cw;
 
 import com.ithinkrok.cw.database.UserCategoryStats;
 import com.ithinkrok.minigames.Game;
-import com.ithinkrok.minigames.MinigamesPlugin;
+import com.ithinkrok.minigames.SpecificPlugin;
 
 import java.util.List;
 
 /**
  * Created by paul on 31/12/15.
  */
-public class CWPlugin extends MinigamesPlugin {
+public class CWPlugin extends SpecificPlugin {
 
     Game minigame;
 

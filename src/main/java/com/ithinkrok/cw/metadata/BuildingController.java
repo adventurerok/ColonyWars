5bb223f706726194f351175525b69fd0bf9ffff0
paul <pauldurbz@gmail.com>
2016-01-31 21:36:43 +0000

    Fixed an NPE when a schematic without a centre block was built

--- src/main/java/com/ithinkrok/cw/metadata/BuildingController.java
+++ src/main/java/com/ithinkrok/cw/metadata/BuildingController.java
@@ -34,17 +34,17 @@ public class BuildingController extends Metadata implements Listener, LocationCh
 
     private final GameGroup gameGroup;
 
-    private HashMap<PastedSchematic, Building> buildings = new HashMap<>();
-    private HashMap<Location, Building> buildingCentres = new HashMap<>();
+    private final HashMap<PastedSchematic, Building> buildings = new HashMap<>();
+    private final HashMap<Location, Building> buildingCentres = new HashMap<>();
 
-    private String shopLocale;
-    private String shopInfoLocale;
+    private final String shopLocale;
+    private final String shopInfoLocale;
 
     public BuildingController(GameGroup gameGroup) {
         this.gameGroup = gameGroup;
 
         ConfigurationSection config = gameGroup.getSharedObject("building_controller_metadata");
-        if(config == null) config = new MemoryConfiguration();
+        if (config == null) config = new MemoryConfiguration();
 
         shopLocale = config.getString("shop_name_locale", "building.shop.name");
         shopInfoLocale = config.getString("shop_description_locale", "building.shop.desc");
@@ -52,7 +52,7 @@ public class BuildingController extends Metadata implements Listener, LocationCh
 
     @Override
     public void removed() {
-        for(Building building : buildings.values()) {
+        for (Building building : buildings.values()) {
             building.getSchematic().removed();
         }
     }
@@ -67,14 +67,78 @@ public class BuildingController extends Metadata implements Listener, LocationCh
         return true;
     }
 
+    public Building getBuilding(Location center) {
+        return buildingCentres.get(center);
+    }
+
+    public boolean buildBuilding(String name, TeamIdentifier team, Location location, int rotation, boolean instant) {
+        SchematicOptions options = createSchematicOptions(team, instant);
+
+        Schematic schem = gameGroup.getSchematic(name);
+        GameMap map = gameGroup.getCurrentMap();
+
+        PastedSchematic pasted =
+                SchematicPaster.buildSchematic(schem, map, location, this, gameGroup, gameGroup, rotation, options);
+
+        if (pasted == null) return false;
+
+        Building building = new Building(name, team, pasted);
+        addBuilding(building);
+
+        return true;
+    }
+
+    private SchematicOptions createSchematicOptions(TeamIdentifier team, boolean instant) {
+        SchematicOptions options = new SchematicOptions(gameGroup.getSharedObject("schematic_options"));
+        options.withOverrideDyeColor(team.getDyeColor());
+        options.withMapBoundsCheck(false); //we do our own Map bounds check
+
+        if (instant) options.withBuildSpeed(-1);
+
+        options.withDefaultListener(BuildingController.getOrCreate(gameGroup));
+
+        return options;
+    }
+
+    public void addBuilding(Building building) {
+        buildings.put(building.getSchematic(), building);
+
+        if (building.getSchematic().getCenterBlock() != null) {
+            buildingCentres.put(building.getSchematic().getCenterBlock(), building);
+        }
+
+        getTeamBuildingStats(building).buildingStarted(building);
+
+        if (building.getSchematic().isFinished()) {
+            onSchematicFinished(new SchematicFinishedEvent(building.getSchematic()));
+        }
+    }
+
+    public static BuildingController getOrCreate(GameGroup gameGroup) {
+        BuildingController result = gameGroup.getMetadata(BuildingController.class);
+
+        if (result == null) {
+            result = new BuildingController(gameGroup);
+            gameGroup.setMetadata(result);
+        }
+
+        return result;
+    }
+
+    private CWTeamStats getTeamBuildingStats(Building building) {
+        Team team = gameGroup.getTeam(building.getTeamIdentifier());
+
+        return CWTeamStats.getOrCreate(team);
+    }
+
     @MinigamesEventHandler
     public void onSchematicFinished(SchematicFinishedEvent event) {
         Building building = buildings.get(event.getSchematic());
-        if(building == null) return;
+        if (building == null || building.getCenterBlock() == null) return;
 
         Location holo1 = building.getCenterBlock().clone().add(0.5d, 2.2d, 0.5d);
-        Hologram hologram1 = HologramAPI.createHologram(holo1, gameGroup.getLocale(shopLocale, building
-                .getBuildingName()));
+        Hologram hologram1 =
+                HologramAPI.createHologram(holo1, gameGroup.getLocale(shopLocale, building.getBuildingName()));
         hologram1.spawn();
         building.getSchematic().addHologram(hologram1);
 
@@ -89,60 +153,6 @@ public class BuildingController extends Metadata implements Listener, LocationCh
         gameGroup.teamEvent(new BuildingBuiltEvent(team, building));
     }
 
-    public Building getBuilding(Location center) {
-        return buildingCentres.get(center);
-    }
-
-    public void addBuilding(Building building) {
-        buildings.put(building.getSchematic(), building);
-
-        if(building.getSchematic().getCenterBlock() != null) {
-            buildingCentres.put(building.getSchematic().getCenterBlock(), building);
-        }
-
-        getTeamBuildingStats(building).buildingStarted(building);
-
-        if(building.getSchematic().isFinished()) {
-            onSchematicFinished(new SchematicFinishedEvent(building.getSchematic()));
-        }
-    }
-
-    public boolean buildBuilding(String name, TeamIdentifier team, Location location, int rotation, boolean instant) {
-        SchematicOptions options = createSchematicOptions(team, instant);
-
-        Schematic schem = gameGroup.getSchematic(name);
-        GameMap map = gameGroup.getCurrentMap();
-
-        PastedSchematic pasted = SchematicPaster
-                .buildSchematic(schem, map, location, this, gameGroup, gameGroup, rotation,
-                        options);
-
-        if(pasted == null) return false;
-
-        Building building = new Building(name, team, pasted);
-        addBuilding(building);
-
-        return true;
-    }
-
-    private SchematicOptions createSchematicOptions(TeamIdentifier team, boolean instant) {
-        SchematicOptions options = new SchematicOptions(gameGroup.getSharedObject("schematic_options"));
-        options.withOverrideDyeColor(team.getDyeColor());
-        options.withMapBoundsCheck(false); //we do our own Map bounds check
-
-        if(instant) options.withBuildSpeed(-1);
-
-        options.withDefaultListener(BuildingController.getOrCreate(gameGroup));
-
-        return options;
-    }
-
-    private CWTeamStats getTeamBuildingStats(Building building) {
-        Team team = gameGroup.getTeam(building.getTeamIdentifier());
-
-        return CWTeamStats.getOrCreate(team);
-    }
-
     @MinigamesEventHandler
     public void onSchematicDestroyed(SchematicDestroyedEvent event) {
         Building building = buildings.remove(event.getSchematic());
@@ -151,22 +161,10 @@ public class BuildingController extends Metadata implements Listener, LocationCh
         getTeamBuildingStats(building).buildingRemoved(building);
     }
 
-    public static BuildingController getOrCreate(GameGroup gameGroup) {
-        BuildingController result = gameGroup.getMetadata(BuildingController.class);
-
-        if(result == null) {
-            result = new BuildingController(gameGroup);
-            gameGroup.setMetadata(result);
-        }
-
-        return result;
-    }
-
     @Override
     /**
      * Checks if the location is part of a building
-     */
-    public boolean check(Location loc) {
+     */ public boolean check(Location loc) {
         return gameGroup.getCurrentMap().canPaste(new BoundingBox(loc.toVector(), loc.toVector()));
     }
 
@@ -174,10 +172,10 @@ public class BuildingController extends Metadata implements Listener, LocationCh
     public boolean canPaste(BoundingBox bounds) {
         GameMap map = gameGroup.getCurrentMap();
 
-        for(int x = bounds.min.getBlockX(); x <= bounds.max.getBlockX(); ++x) {
-            for(int y = bounds.min.getBlockY(); y <= bounds.max.getBlockY(); ++y) {
-                for(int z = bounds.min.getBlockZ(); z <= bounds.max.getBlockZ(); ++z) {
-                    switch(map.getBlock(x, y, z).getType()) {
+        for (int x = bounds.min.getBlockX(); x <= bounds.max.getBlockX(); ++x) {
+            for (int y = bounds.min.getBlockY(); y <= bounds.max.getBlockY(); ++y) {
+                for (int z = bounds.min.getBlockZ(); z <= bounds.max.getBlockZ(); ++z) {
+                    switch (map.getBlock(x, y, z).getType()) {
                         case BEDROCK:
                         case BARRIER:
                             return false;

7063cbab5f1aa072e7cccc4616b00292e6103e1a
paul <pauldurbz@gmail.com>
2018-04-09 17:46:06 +0100

    Join the game automatically when you join the gamegroup

--- src/main/java/com/ithinkrok/cw/gamestate/CWTutorialGameListener.java
+++ src/main/java/com/ithinkrok/cw/gamestate/CWTutorialGameListener.java
@@ -1,11 +1,30 @@
 package com.ithinkrok.cw.gamestate;
 
+import com.ithinkrok.cw.command.CWCommand;
 import com.ithinkrok.minigames.api.GameGroup;
+import com.ithinkrok.minigames.api.GameState;
 import com.ithinkrok.minigames.api.Kit;
+import com.ithinkrok.minigames.api.event.ListenerLoadedEvent;
+import com.ithinkrok.minigames.api.event.user.game.UserJoinEvent;
 import com.ithinkrok.minigames.api.team.Team;
 import com.ithinkrok.minigames.api.user.User;
+import com.ithinkrok.util.config.Config;
+import com.ithinkrok.util.event.CustomEventHandler;
 
 public class CWTutorialGameListener extends CWInGameListener {
+
+
+    protected String defaultKit;
+
+
+    @Override
+    @CustomEventHandler
+    public void onListenerLoaded(ListenerLoadedEvent<GameGroup, GameState> event) {
+        super.onListenerLoaded(event);
+
+        Config config = event.getConfigOrEmpty();
+        defaultKit = config.getString("default_kit", null);
+    }
 
 
     @Override
@@ -35,5 +54,21 @@ public class CWTutorialGameListener extends CWInGameListener {
 
         died.setTeam(team);
         died.setKit(kit);
+    }
+
+    @CustomEventHandler
+    @Override
+    public void onUserJoined(UserJoinEvent event) {
+        if(event.getUser().isInGame()) return;
+
+        if(defaultKit != null) {
+            event.getUser().setKit(event.getGameGroup().getKit(defaultKit));
+        }
+
+        CWCommand.UserRejoinEvent rejoin = new CWCommand.UserRejoinEvent(event.getUser());
+        rejoin.setCancelled(true);
+        rejoin.getGameGroup().userEvent(rejoin);
+
+        super.onUserJoined(event);
     }
 }
